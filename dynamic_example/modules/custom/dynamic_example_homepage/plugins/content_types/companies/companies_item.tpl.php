e23f52d47eb3cab48e7ed52e13cafd4903a36e72
Amitaibu <amitai@gizra.com>
2016-07-20 14:43:26 +0300

    Add companies plugin.

---
+++ dynamic_example/modules/custom/dynamic_example_homepage/plugins/content_types/companies/companies_item.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Companies item.
+ */
+?>
+
+<div class="item">
+  <div class="ui tiny image">
+    <img src="<?php print $image; ?>">
+  </div>
+  <div class="middle aligned content">
+    <a href="<?php print $link; ?>" class="header"><?php print $title; ?></a>
+  </div>
+</div>
