a02e815f225872eebed7757e961843d102ffd600
Liat Sade Saadon <liat@gizra.com>
2016-07-21 08:58:56 +0300

    add Card plugin

---
+++ dynamic_example/modules/custom/dynamic_example_homepage/plugins/content_types/cards/cards.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Companies.
+ */
+?>
+
+<div class="ui segment stacked">
+  <div class="ui very relaxed items">
+
+    <?php
+      foreach ($cards as $card) {
+        print $card;
+      }
+    ?>
+
+  </div>
+</div>
