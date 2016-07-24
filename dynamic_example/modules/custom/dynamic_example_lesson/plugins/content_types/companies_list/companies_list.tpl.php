8b2e9efd0375fe775c26abfd8f8ce4cf740fafee
Amitaibu <amitai@gizra.com>
2016-07-24 16:28:41 +0300

    Add plugin

---
+++ dynamic_example/modules/custom/dynamic_example_lesson/plugins/content_types/companies_list/companies_list.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Companies list.
+ */
+?>
+
+<div class="ui segment stacked">
+  <div class="ui very relaxed items">
+
+    <?php foreach ($companies_item as $item) {
+      print $item;
+    }
+    ?>
+
+  </div>
+</div>
+
