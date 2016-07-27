ae8ca9604b6f2e44b5e59e07e55f86a381b21f6c
Liat Sade Saadon <liat@gizra.com>
2016-07-27 15:15:13 +0300

    update files

---
+++ dynamic_example/modules/custom/dynamic_example_lesson2/plugins/content_types/companies_list/companies_list.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Company list
+ */
+?>
+<div class="ui segment stacked">
+  <div class="ui very relaxed items">
+
+    <div class="item">
+      <div class="ui tiny image">
+        <img src="http://placehold.it/150x150">
+      </div>
+      <div class="middle aligned content">
+        <a class="header">12 Years a Slave</a>
+      </div>
+    </div>
+
+  </div>
+</div>
