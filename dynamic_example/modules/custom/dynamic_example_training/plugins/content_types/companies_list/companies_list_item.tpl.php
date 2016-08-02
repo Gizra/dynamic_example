f54998c5d777e96a862181a82be26fd6bfe2b83d
Liat Sade Saadon <liat@gizra.com>
2016-08-02 16:08:06 +0300

    delete files

--- dynamic_example/modules/custom/dynamic_example_training/plugins/content_types/companies_list/companies_list_item.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="item">
-    <div class="ui tiny image">
-        <img src="<?php print $image; ?>">
-    </div>
-    <div class="middle aligned content">
-        <a href="<?php print $url; ?>" class="header"><?php print $title; ?></a>
-    </div>
-</div>
