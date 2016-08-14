757fb72b8b8e1e9f2b56934d6bb44fce60316dd9
Liat Sade Saadon <liat@gizra.com>
2016-08-14 09:24:03 +0300

    add files

---
+++ dynamic_example/modules/custom/dynamic_example_training/plugins/content_types/companies_list/companies_list_item.tpl.php
@@ -0,0 +1,8 @@
+<div class="item">
+    <div class="ui tiny image">
+        <img src="<?php print $image; ?>">
+    </div>
+    <div class="middle aligned content">
+        <a href="<?php print $url; ?>" class="header"><?php print $title; ?></a>
+    </div>
+</div>
